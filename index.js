3c2611f4fb1ae5cd91f05a3173522e4b5927a328
kumar <jyotishgehlot@gmail.com>
2024-06-17 15:24:32 +0530

    complete createBlog route and make some changes in index.ejs and createBlog.ejs

--- index.js
+++ index.js
@@ -5,25 +5,29 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
+let posts = [];
+
 //middlewares
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 //routes
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
-app.post("/submitBlog", (req, res) => {
+app.get("/newBlog", (req, res) => {
     res.render("createBlog.ejs");
 });
 app.post("/createBlog", (req, res) => {
-    console.log(req.body["authorName"]);
-    console.log(req.body["articleTitle"]);
-    console.log(req.body["date"]);
-    console.log(req.body["categories"]);
-    console.log(req.body["blogContent"]);
+    const blogData = req.body;
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    blogData.id = id;
+    posts.push(req.body);
+    console.log(posts);
+    res.redirect("/newBlog");
 });
 
-//server 
+//start server 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}.`);
 });
